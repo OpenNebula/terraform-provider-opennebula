1353eea2e7ee5496898ee85c2909da7846e33524
Christian González <cgonzalez@opennebula.systems>
2019-08-30 09:09:20 +0200

    Add travis

--- .travis/tests/02-oned.sh
+++ .travis/tests/02-oned.sh
@@ -15,8 +15,7 @@
 #--------------------------------------------------------------------------- #
 
 # Set credentials
-sudo su -c "mkdir /var/lib/one/.one" oneadmin
-sudo su -c "echo "oneadmin:opennebula" > /var/lib/one/.one/one_auth" oneadmin
+sudo su -c "echo 'oneadmin:opennebula' > /var/lib/one/.one/one_auth" oneadmin
 
 # Enable dummy drivers
 sudo chmod o+w /etc/one/oned.conf
