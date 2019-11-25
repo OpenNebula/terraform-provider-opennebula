8910cfd7b23fa7923da1913da989d0bc06a77862
cgriggs01 <cgriggs@hashicorp.com>
2019-11-25 10:10:45 -0800

    fmt main.go

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/terraform-providers/terraform-provider-opennebula/opennebula"
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
+	"github.com/terraform-providers/terraform-provider-opennebula/opennebula"
 )
 
 func main() {
