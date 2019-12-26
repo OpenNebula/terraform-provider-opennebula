c1029e5bfa5825ac8446f447236524a09c5a2c3e
Pierre Lafievre <pierre.lafievre@iguanesolutions.com>
2019-12-26 14:21:49 +0100

    add helper to check empty values

--- opennebula/helpers.go
+++ opennebula/helpers.go
@@ -2,8 +2,10 @@ package opennebula
 
 import (
 	"fmt"
-	"github.com/OpenNebula/one/src/oca/go/src/goca/schemas/shared"
+	"reflect"
 	"strings"
+
+	"github.com/OpenNebula/one/src/oca/go/src/goca/schemas/shared"
 )
 
 func inArray(val string, array []string) (index int) {
@@ -59,4 +61,20 @@ func LockLevelToString(lock int) string {
 		return "ALL"
 	}
 	return ""
+}
+
+func isEmptyValue(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.String, reflect.Array, reflect.Map, reflect.Slice:
+		return v.Len() == 0
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Interface, reflect.Ptr:
+		return v.IsNil()
+	}
+	return false
 }
