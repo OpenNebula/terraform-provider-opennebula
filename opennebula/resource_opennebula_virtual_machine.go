OpenNebula/terraform-provider-opennebula

commit 7f2d51ea56c4ec7a133fc6b96a7d603c69934738

B-43: Resource/Virtual_machine: Fix error management

opennebula/resource_opennebula_virtual_machine.go
@@ -221,6 +221,9 @@ func resourceOpennebulaVirtualMachineCreate(d *schema.ResourceData, meta interfa
 		// Instantiate template without creating a persistent copy of the template
 		// Note that the new VM is not pending
 		vmID, err = tc.Instantiate(d.Get("name").(string), d.Get("pending").(bool), vmDef, false)
+		if err != nil {
+			return err
+		}
 	} else {
 		if _, ok := d.GetOk("cpu"); !ok {
 			return fmt.Errorf("cpu is mandatory as template_id is not used")
@@ -236,10 +239,9 @@ func resourceOpennebulaVirtualMachineCreate(d *schema.ResourceData, meta interfa
 
 		// Create VM not in pending state
 		vmID, err = controller.VMs().Create(vmDef, d.Get("pending").(bool))
-	}
-
-	if err != nil {
-		return err
+		if err != nil {
+			return err
+		}
 	}
 
 	d.SetId(fmt.Sprintf("%v", vmID))
@@ -500,14 +502,14 @@ func waitForVmState(d *schema.ResourceData, meta interface{}, state string) (int
 			vm, err = vmc.Info(false)
 			if err != nil {
 				if strings.Contains(err.Error(), "Error getting") {
-					return vm, "notfound", nil
+					return vm, "notfound", err
 				}
 				return vm, "", err
 			}
 			vmState, vmLcmState, err := vm.State()
 			if err != nil {
 				if strings.Contains(err.Error(), "Error getting") {
-					return vm, "notfound", nil
+					return vm, "notfound", err
 				}
 				return vm, "", err
 			}

opennebula/resource_opennebula_virtual_machine_test.go
@@ -135,7 +135,7 @@ func testAccCheckVirtualMachineDestroy(s *terraform.State) error {
 		if vm != nil {
 			vmState, _, _ := vm.State()
 			if vmState != 6 {
-				return fmt.Errorf("Expected virtual machine %s to have been destroyed", rs.Primary.ID)
+				return fmt.Errorf("Expected virtual machine %s to have been destroyed. vmState: %v", rs.Primary.ID, vmState)
 			}
 		}
 	}

commit 568904fd8b852a5d71b0d2951b3589fdd995e6b8

B-50: Fix regression on Virtual Machine test
Regression introduced by 7f2d51ea56c4ec7a133fc6b96a7d603c69934738

opennebula/resource_opennebula_virtual_machine.go
@@ -502,14 +502,18 @@ func waitForVmState(d *schema.ResourceData, meta interface{}, state string) (int
 			vm, err = vmc.Info(false)
 			if err != nil {
 				if strings.Contains(err.Error(), "Error getting") {
-					return vm, "notfound", err
+					// Do not return an error here as it is excpected if the VM is already in DONE state
+					// after its destruction
+					return vm, "notfound", nil
 				}
 				return vm, "", err
 			}
 			vmState, vmLcmState, err := vm.State()
 			if err != nil {
 				if strings.Contains(err.Error(), "Error getting") {
-					return vm, "notfound", err
+					// Do not return an error here as it is excpected if the VM is already in DONE state
+					// after its destruction
+					return vm, "notfound", nil
 				}
 				return vm, "", err
 			}