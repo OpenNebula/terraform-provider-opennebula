b5c429e0090773fee3f2508631a75c9894f17502
Pierre Lafievre <pierre.lafievre@iguanesolutions.com>
2021-08-31 03:15:27 +0200

    F #65: image: add acceptance test

--- opennebula/resource_opennebula_image_test.go
+++ opennebula/resource_opennebula_image_test.go
@@ -49,6 +49,36 @@ func TestAccImage(t *testing.T) {
 				),
 			},
 			{
+				Config: testAccImageConfigDatablockLocked,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "name", "test-image-datablock"),
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "datastore_id", "1"),
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "persistent", "true"),
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "type", "DATABLOCK"),
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "size", "128"),
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "computed_size", "128"),
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "dev_prefix", "vd"),
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "driver", "qcow2"),
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "permissions", "742"),
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "tags.%", "2"),
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "tags.env", "prod"),
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "tags.customer", "test"),
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "timeout", "5"),
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "lock", "USE"),
+					resource.TestCheckResourceAttrSet("opennebula_image.testimage", "uid"),
+					resource.TestCheckResourceAttrSet("opennebula_image.testimage", "gid"),
+					resource.TestCheckResourceAttrSet("opennebula_image.testimage", "uname"),
+					resource.TestCheckResourceAttrSet("opennebula_image.testimage", "gname"),
+					testAccCheckImagePermissions(&shared.Permissions{
+						OwnerU: 1,
+						OwnerM: 1,
+						OwnerA: 1,
+						GroupU: 1,
+						OtherM: 1,
+					}, "test-image-datablock"),
+				),
+			},
+			{
 				Config: testAccImageConfigDatablockUpdate,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("opennebula_image.testimage", "name", "test-image-datablock"),
@@ -65,6 +95,7 @@ func TestAccImage(t *testing.T) {
 					resource.TestCheckResourceAttr("opennebula_image.testimage", "tags.customer", "test"),
 					resource.TestCheckResourceAttr("opennebula_image.testimage", "tags.version", "2"),
 					resource.TestCheckResourceAttr("opennebula_image.testimage", "timeout", "10"),
+					resource.TestCheckResourceAttr("opennebula_image.testimage", "lock", "UNLOCK"),
 					resource.TestCheckResourceAttrSet("opennebula_image.testimage", "uid"),
 					resource.TestCheckResourceAttrSet("opennebula_image.testimage", "gid"),
 					resource.TestCheckResourceAttrSet("opennebula_image.testimage", "uname"),
@@ -151,6 +182,26 @@ resource "opennebula_image" "testimage" {
 }
 `
 
+var testAccImageConfigDatablockLocked = `
+resource "opennebula_image" "testimage" {
+   name = "test-image-datablock"
+   description = "Terraform datablock"
+   datastore_id = 1
+   persistent = true
+   type = "DATABLOCK"
+   size = "128"
+   dev_prefix = "vd"
+   permissions = "742"
+   driver = "qcow2"
+   tags = {
+     env = "prod"
+     customer = "test"
+   }
+   timeout = 5
+   lock = "USE"
+}
+`
+
 var testAccImageConfigDatablockUpdate = `
 resource "opennebula_image" "testimage" {
    name = "test-image-datablock"
@@ -168,5 +219,6 @@ resource "opennebula_image" "testimage" {
      version = "2"
    }
    timeout = 10
+   lock = "UNLOCK"
 }
 `
