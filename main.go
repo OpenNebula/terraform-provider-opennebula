b26a25b2ff60977b3f76bd59b031a02a032b62d8
Pierre Lafievre <pierre.lafievre@iguanesolutions.com>
2021-12-14 18:37:16 +0100

    F #137: compilation fixes

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"github.com/OpenNebula/terraform-provider-opennebula/opennebula"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
+		ProviderFunc: func() *schema.Provider {
 			return opennebula.Provider()
 		},
 	})
