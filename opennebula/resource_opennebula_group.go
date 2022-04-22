7bd5d2d13f25cf397542d23b802bb12b638a16d4
Pierre Lafievre <pierre.lafievre@iguanesolutions.com>
2022-04-22 18:13:14 +0200

    F #248: group: add sunstone and tags sections

--- opennebula/resource_opennebula_group.go
+++ opennebula/resource_opennebula_group.go
@@ -1,13 +1,17 @@
 package opennebula
 
 import (
+	"encoding/xml"
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 
 	"github.com/OpenNebula/one/src/oca/go/src/goca"
+	dyn "github.com/OpenNebula/one/src/oca/go/src/goca/dynamic"
+	"github.com/OpenNebula/one/src/oca/go/src/goca/parameters"
 )
 
 func resourceOpennebulaGroup() *schema.Resource {
@@ -27,9 +31,11 @@ func resourceOpennebulaGroup() *schema.Resource {
 				Description: "Name of the Group",
 			},
 			"template": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "Group template content, in OpenNebula XML or String format",
+				Type:          schema.TypeString,
+				Optional:      true,
+				Description:   "Group template content, in OpenNebula XML or String format",
+				Deprecated:    "use other schema sections",
+				ConflictsWith: []string{"sunstone", "tags"},
 			},
 			"delete_on_destruction": {
 				Type:        schema.TypeBool,
@@ -58,6 +64,41 @@ func resourceOpennebulaGroup() *schema.Resource {
 				Deprecated: "use opennebula_group_admins resource instead.",
 			},
 			"quotas": quotasSchema(),
+			"sunstone": {
+				Type:        schema.TypeSet,
+				Optional:    true,
+				Description: "Allow users and group admins to access specific views",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"default_view": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "Default Sunstone view for regular users",
+						},
+						"views": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "List of available views for regular users",
+						},
+						"group_admin_default_view": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "Default Sunstone view for group admin users",
+						},
+						"group_admin_views": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "List of available views for the group admins",
+						},
+					},
+				},
+				ConflictsWith: []string{"template"},
+			},
+			"tags": func() *schema.Schema {
+				s := tagsSchema()
+				s.ConflictsWith = []string{"template"}
+				return s
+			}(),
 		},
 	}
 }
@@ -98,15 +139,6 @@ func resourceOpennebulaGroupCreate(d *schema.ResourceData, meta interface{}) err
 
 	gc := controller.Group(groupID)
 
-	// add template description
-	if d.Get("template") != "" {
-		// Erase previous template
-		err = gc.Update(d.Get("template").(string), 0)
-		if err != nil {
-			return err
-		}
-	}
-
 	// add users if list provided
 	if userids, ok := d.GetOk("users"); ok {
 		userlist := userids.([]interface{})
@@ -141,7 +173,67 @@ func resourceOpennebulaGroupCreate(d *schema.ResourceData, meta interface{}) err
 		}
 	}
 
+	// template management
+
+	// add template description
+	if d.Get("template") != "" {
+		// Erase previous template
+		err = gc.Update(d.Get("template").(string), 0)
+		if err != nil {
+			return err
+		}
+	}
+
+	tpl := dyn.NewTemplate()
+
+	sunstone := d.Get("sunstone").(*schema.Set).List()
+	if len(sunstone) > 0 {
+		sunstoneVec := makeSunstoneVec(sunstone[0].(map[string]interface{}))
+		tpl.Elements = append(tpl.Elements, sunstoneVec)
+	}
+
+	tagsInterface := d.Get("tags").(map[string]interface{})
+	for k, v := range tagsInterface {
+		tpl.AddPair(strings.ToUpper(k), v)
+	}
+
+	if len(tpl.Elements) > 0 {
+		err = gc.Update(tpl.String(), parameters.Merge)
+		if err != nil {
+			return err
+		}
+	}
+
 	return resourceOpennebulaGroupRead(d, meta)
+}
+
+func makeSunstoneVec(sunstoneConfig map[string]interface{}) *dyn.Vector {
+
+	vector := dyn.Vector{
+		XMLName: xml.Name{Local: "SUNSTONE"},
+	}
+
+	defaultView := sunstoneConfig["default_view"].(string)
+	if len(defaultView) > 0 {
+		vector.AddPair("DEFAULT_VIEW", defaultView)
+	}
+
+	views := sunstoneConfig["views"].(string)
+	if len(views) > 0 {
+		vector.AddPair("VIEWS", views)
+	}
+
+	groupAdminDefaultView := sunstoneConfig["group_admin_default_view"].(string)
+	if len(groupAdminDefaultView) > 0 {
+		vector.AddPair("GROUP_ADMIN_DEFAULT_VIEW", groupAdminDefaultView)
+	}
+
+	groupAdminViews := sunstoneConfig["group_admin_views"].(string)
+	if len(groupAdminViews) > 0 {
+		vector.AddPair("GROUP_ADMIN_VIEWS", groupAdminViews)
+	}
+
+	return &vector
 }
 
 func resourceOpennebulaGroupRead(d *schema.ResourceData, meta interface{}) error {
@@ -198,21 +290,77 @@ func resourceOpennebulaGroupRead(d *schema.ResourceData, meta interface{}) error
 		}
 	}
 
-	return nil
-}
-
-func resourceOpennebulaGroupUpdate(d *schema.ResourceData, meta interface{}) error {
-	gc, err := getGroupController(d, meta)
+	err = flattenGroupTemplate(d, &group.Template)
 	if err != nil {
 		return err
 	}
 
-	if d.HasChange("template") {
-		// Erase previous template
-		err = gc.Update(d.Get("template").(string), 0)
+	return nil
+}
+
+func flattenGroupTemplate(d *schema.ResourceData, groupTpl *dyn.Template) error {
+
+	tags := make(map[string]interface{})
+	for i, _ := range groupTpl.Elements {
+
+		switch e := groupTpl.Elements[i].(type) {
+		case *dyn.Pair:
+
+			// Get only tags described in the configuration
+			if tagsInterface, ok := d.GetOk("tags"); ok {
+				var err error
+				for k, _ := range tagsInterface.(map[string]interface{}) {
+					tags[k], err = groupTpl.GetStr(strings.ToUpper(k))
+					if err != nil {
+						return err
+					}
+				}
+
+			}
+		case *dyn.Vector:
+			switch e.Key() {
+			case "SUNSTONE":
+				defaultView, _ := e.GetStr("DEFAULT_VIEW")
+				views, _ := e.GetStr("VIEWS")
+				groupAdminDefaultView, _ := e.GetStr("GROUP_ADMIN_DEFAULT_VIEW")
+				groupAdminViews, _ := e.GetStr("GROUP_ADMIN_VIEWS")
+
+				sunstoneConfig := []map[string]interface{}{
+					{
+						"default_view":             defaultView,
+						"views":                    views,
+						"group_admin_default_view": groupAdminDefaultView,
+						"group_admin_views":        groupAdminViews,
+					},
+				}
+
+				err := d.Set("sunstone", sunstoneConfig)
+				if err != nil {
+					return err
+				}
+			default:
+				log.Printf("[DEBUG] ignored: %s", e)
+			}
+
+		}
+
+	}
+
+	if len(tags) > 0 {
+		err := d.Set("tags", tags)
 		if err != nil {
 			return err
 		}
+	}
+
+	return nil
+}
+
+func resourceOpennebulaGroupUpdate(d *schema.ResourceData, meta interface{}) error {
+
+	gc, err := getGroupController(d, meta)
+	if err != nil {
+		return err
 	}
 
 	if d.HasChange("quotas") {
@@ -225,6 +373,67 @@ func resourceOpennebulaGroupUpdate(d *schema.ResourceData, meta interface{}) err
 			if err != nil {
 				return err
 			}
+		}
+	}
+
+	// template management
+
+	group, err := gc.Info(false)
+	if err != nil {
+		return err
+	}
+
+	if d.HasChange("template") {
+		// Erase previous template
+		err = gc.Update(d.Get("template").(string), 0)
+		if err != nil {
+			return err
+		}
+	}
+
+	update := false
+	newTpl := group.Template
+
+	if d.HasChange("sunstone") {
+		newTpl.Del("SUNSTONE")
+
+		sunstone := d.Get("sunstone").(*schema.Set).List()
+		if len(sunstone) > 0 {
+			sunstoneVec := makeSunstoneVec(sunstone[0].(map[string]interface{}))
+			newTpl.Elements = append(newTpl.Elements, sunstoneVec)
+		}
+
+		update = true
+	}
+
+	if d.HasChange("tags") {
+
+		oldTagsIf, newTagsIf := d.GetChange("tags")
+		oldTags := oldTagsIf.(map[string]interface{})
+		newTags := newTagsIf.(map[string]interface{})
+
+		// delete tags
+		for k, _ := range oldTags {
+			_, ok := newTags[k]
+			if ok {
+				continue
+			}
+			newTpl.Del(strings.ToUpper(k))
+		}
+
+		// add/update tags
+		for k, v := range newTags {
+			newTpl.Del(strings.ToUpper(k))
+			newTpl.AddPair(strings.ToUpper(k), v)
+		}
+
+		update = true
+	}
+
+	if update {
+		err = gc.Update(newTpl.String(), parameters.Replace)
+		if err != nil {
+			return err
 		}
 	}
 
